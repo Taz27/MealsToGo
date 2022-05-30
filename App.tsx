7d367e15cb4b79f165157b81251df9c68ea70468
Taran Mand <tsmand@yahoo.com>
2022-05-30 18:32:08 +1000

    add Searchbar and add/update react types

--- App.tsx
+++ App.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { Searchbar } from 'react-native-paper';
 import { StyleSheet, Text, View, SafeAreaView, StatusBar, Platform } from 'react-native';
 
 const isAndroid: boolean = Platform.OS === 'android';
 
 export default function App() {
+  const [searchQuery, setSearchQuery] = useState<string>('');
+
   return (
     <>
       <SafeAreaView style={styles.container}>
         <View style={styles.header}>
-          <Text>Search</Text>
+          <Searchbar placeholder="Search" onChangeText={setSearchQuery} value={searchQuery} />
         </View>
         <View style={styles.content}>
           <Text>List</Text>

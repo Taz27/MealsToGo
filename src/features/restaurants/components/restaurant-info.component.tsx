f0e730eeb3acedcf056097c0cc5ac56081dd7d31
Taran Mand <tsmand@yahoo.com>
2022-06-11 19:54:26 +1000

    create types in RestaurantInfo

--- src/features/restaurants/components/restaurant-info.component.tsx
+++ src/features/restaurants/components/restaurant-info.component.tsx
@@ -1,6 +1,32 @@
 import React from 'react';
 import { Text } from 'react-native';
 
-export const RestaurantInfo: React.FC = () => {
-  return <Text>Restaurant Info!</Text>;
+type Restaurant = {
+  name: string;
+  icon: string;
+  photos: string[];
+  address: string;
+  isOpenNow: boolean;
+  rating: number;
+  isClosedTemporarily: boolean;
+};
+
+interface RestaurantProps {
+  restaurant: Restaurant;
+}
+
+export const RestaurantInfo: React.FC<RestaurantProps> = ({ restaurant = {} }) => {
+  const {
+    name = 'Some Restaurant',
+    icon,
+    photos = [
+      'https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg',
+    ],
+    address = '100 some random street',
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily,
+  } = restaurant;
+
+  return <Text>{name}</Text>;
 };

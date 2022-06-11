6cebe41c3507e14af96cb01987e6bd583d10f406
Taran Mand <tsmand@yahoo.com>
2022-06-11 19:34:10 +1000

    create RestaurantInfo and RestaurantsScreen components

--- App.tsx
+++ App.tsx
@@ -1,40 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import { Searchbar } from 'react-native-paper';
-import { StyleSheet, Text, View, SafeAreaView, StatusBar, Platform } from 'react-native';
-
-const isAndroid: boolean = Platform.OS === 'android';
+import { RestaurantsScreen } from './src/features/restaurants/screens/restaurants.screen';
 
 export default function App() {
-  const [searchQuery, setSearchQuery] = useState<string>('');
-
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-          <Searchbar placeholder="Search" onChangeText={setSearchQuery} value={searchQuery} />
-        </View>
-        <View style={styles.content}>
-          <Text>List</Text>
-        </View>
-      </SafeAreaView>
+      <RestaurantsScreen />
       <ExpoStatusBar style="auto" />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: isAndroid ? StatusBar.currentHeight : 0,
-    backgroundColor: 'white',
-  },
-  search: {
-    padding: 16,
-  },
-  content: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: 'blue',
-  },
-});
---
+++ src/features/restaurants/screens/restaurants.screen.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { Searchbar } from 'react-native-paper';
+import { StyleSheet, View, SafeAreaView, StatusBar, Platform } from 'react-native';
+import { RestaurantInfo } from '../components/restaurant-info.component';
+
+const isAndroid: boolean = Platform.OS === 'android';
+
+export const RestaurantsScreen: React.FC = () => {
+  const [searchQuery, setSearchQuery] = useState<string>('');
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.search}>
+        <Searchbar placeholder="Search" onChangeText={setSearchQuery} value={searchQuery} />
+      </View>
+      <View style={styles.content}>
+        <RestaurantInfo />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: isAndroid ? StatusBar.currentHeight : 0,
+    backgroundColor: 'white',
+  },
+  search: {
+    padding: 16,
+  },
+  content: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: 'blue',
+  },
+});

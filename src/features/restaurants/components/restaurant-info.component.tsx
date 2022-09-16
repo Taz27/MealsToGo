aa80dec2eaf0d884598ed838688fbe48f71b83fe
Taran Mand <tsmand@yahoo.com>
2022-09-16 19:08:21 +1000

    add restaurant info card

--- src/features/restaurants/components/restaurant-info.component.tsx
+++ src/features/restaurants/components/restaurant-info.component.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, StyleSheet } from 'react-native';
+import { Card } from 'react-native-paper';
 
 type Restaurant = {
   name: string;
@@ -28,5 +29,23 @@ export const RestaurantInfo: React.FC<RestaurantProps> = ({ restaurant = {} }) =
     isClosedTemporarily,
   } = restaurant;
 
-  return <Text>{name}</Text>;
+  return (
+    <Card elevation={5} style={styles.card}>
+      <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
+      <Text style={styles.title}>{name}</Text>
+    </Card>
+  );
 };
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: 'white',
+  },
+  cover: {
+    backgroundColor: 'white',
+    padding: 20,
+  },
+  title: {
+    padding: 16,
+  },
+});

8c6f3e78916862bec8d7a303b82c7b91b0a11918
Taran Mand <tsmand@yahoo.com>
2022-12-30 14:54:51 +1100

    add theme provider

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { ThemeProvider } from 'styled-components/native';
 import { RestaurantsScreen } from './src/features/restaurants/screens/restaurants.screen';
+import { theme } from './src/infra/theme';
 
 export default function App() {
   return (
     <>
-      <RestaurantsScreen />
+      <ThemeProvider theme={theme}>
+        <RestaurantsScreen />
+      </ThemeProvider>
+
       <ExpoStatusBar style="auto" />
     </>
   );

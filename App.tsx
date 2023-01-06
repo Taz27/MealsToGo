f95112a6548d3984f1f51c70f65c0d13c4f13829
Taran Mand <tsmand@yahoo.com>
2023-01-06 19:23:01 +1100

    wrap app inside restaurant context provider

--- App.tsx
+++ App.tsx
@@ -8,11 +8,11 @@ import { Ionicons } from '@expo/vector-icons';
 import { RestaurantsScreen } from './src/features/restaurants/screens/restaurants.screen';
 import { SafeArea } from './src/components/utils/safearea.component';
 import { theme } from './src/infra/theme';
+import { RestaurantsContextProvider } from './src/services/restaurants/restaurants.context';
 
 import { useFonts as useOswald, Oswald_400Regular } from '@expo-google-fonts/oswald';
 import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
 import { OpaqueColorValue, Text } from 'react-native';
-import './src/services/restaurants/restaurants.service';
 
 function MapScreen() {
   return (
@@ -72,13 +72,15 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <NavigationContainer>
-          <Tab.Navigator screenOptions={createScreenOptions}>
-            <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
-            <Tab.Screen name="Map" component={MapScreen} />
-            <Tab.Screen name="Settings" component={SettingsScreen} />
-          </Tab.Navigator>
-        </NavigationContainer>
+        <RestaurantsContextProvider>
+          <NavigationContainer>
+            <Tab.Navigator screenOptions={createScreenOptions}>
+              <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+              <Tab.Screen name="Map" component={MapScreen} />
+              <Tab.Screen name="Settings" component={SettingsScreen} />
+            </Tab.Navigator>
+          </NavigationContainer>
+        </RestaurantsContextProvider>
       </ThemeProvider>
 
       <ExpoStatusBar style="auto" />

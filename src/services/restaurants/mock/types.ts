a8508994ea1386c418cfc827747c6b96f00776c7
Taran Mand <tsmand@yahoo.com>
2023-01-06 17:02:04 +1100

    add RestaurantTransformed types

---
+++ src/services/restaurants/mock/types.ts
@@ -0,0 +1,46 @@
+export interface RestuarantTransformed {
+  businessStatus: string;
+  geometry: {
+    location: {
+      lat: number;
+      lng: number;
+    };
+    viewport: {
+      northeast: {
+        lat: number;
+        lng: number;
+      };
+      southwest: {
+        lat: number;
+        lng: number;
+      };
+    };
+  };
+  icon: string;
+  name: string;
+  openingHours: {
+    openNow: boolean;
+  };
+  photos: [
+    {
+      height: number;
+      htmlAttributions: string[];
+      photoReference: string;
+      width: number;
+    }
+  ];
+  placeId: string;
+  plusCode: {
+    compoundCode: string;
+    globalCode: string;
+  };
+  priceLevel: number;
+  rating: number;
+  reference: string;
+  scope: string;
+  types: string[];
+  userRatingsTotal: number;
+  vicinity: string;
+  isOpenNow: boolean;
+  isClosedTemporarily: boolean;
+}

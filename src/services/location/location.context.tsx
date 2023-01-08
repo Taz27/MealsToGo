7f337b4ac38c502248d49ac28bd5b4e7551bcbd8
Taran Mand <tsmand@yahoo.com>
2023-01-08 19:37:58 +1100

    add location context

---
+++ src/services/location/location.context.tsx
@@ -0,0 +1,65 @@
+import React, { useState, useEffect, createContext } from 'react';
+
+import { locationRequest, locationTransform } from './location.service';
+
+export const LocationContext = createContext<{
+  isLoading: boolean;
+  error: Error | null;
+  location: { lat: number; lng: number } | null;
+  search: (val: string) => void;
+  keyword: string;
+}>({
+  isLoading: false,
+  error: null,
+  location: null,
+  search: () => undefined,
+  keyword: '',
+});
+
+export const LocationContextProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [keyword, setKeyword] = useState<string>('san francisco');
+  const [location, setLocation] = useState<{ lat: number; lng: number } | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<Error | null>(null);
+
+  const onSearch = (searchKeyword = 'antwerp') => {
+    // console.log(searchKeyword);
+
+    setIsLoading(true);
+    setKeyword(searchKeyword.trim());
+    if (!searchKeyword.length) {
+      // don't do anything
+      return;
+    }
+    locationRequest(searchKeyword.toLowerCase())
+      .then(locationTransform)
+      .then(res => {
+        setIsLoading(false);
+        setLocation(res);
+        console.log({ res });
+      })
+      .catch(err => {
+        setIsLoading(false);
+        setError(err);
+        console.log({ err });
+      });
+  };
+
+  useEffect(() => {
+    onSearch();
+  }, []);
+
+  return (
+    <LocationContext.Provider
+      value={{
+        isLoading,
+        error,
+        location,
+        search: onSearch,
+        keyword,
+      }}
+    >
+      {children}
+    </LocationContext.Provider>
+  );
+};

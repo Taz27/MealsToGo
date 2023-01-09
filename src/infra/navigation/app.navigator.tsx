3cb4d6011e290277c7f6653058e71d0f96021b04
Taran Mand <tsmand@yahoo.com>
2023-01-09 20:48:43 +1100

    update app navigator

--- src/infra/navigation/app.navigator.tsx
+++ src/infra/navigation/app.navigator.tsx
@@ -4,7 +4,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
 import { OpaqueColorValue, Text } from 'react-native';
 
-import { RestaurantsScreen } from '../../features/restaurants/screens/restaurants.screen';
+import { RestaurantsNavigator } from './restaurants.navigator';
 import { SafeArea } from '../../components/utils/safearea.component';
 
 const Tab = createBottomTabNavigator();
@@ -46,13 +46,14 @@ const createScreenOptions = ({ route }: { route: RouteProp<ParamListBase, string
     }) => <Ionicons name={iconName} size={size} color={color} />,
     tabBarActiveTintColor: 'tomato',
     tabBarInactiveTintColor: 'gray',
+    headerShown: false,
   };
 };
 
 export const AppNavigator = () => (
   <NavigationContainer>
     <Tab.Navigator screenOptions={createScreenOptions}>
-      <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+      <Tab.Screen name="Restaurants" component={RestaurantsNavigator} />
       <Tab.Screen name="Map" component={MapScreen} />
       <Tab.Screen name="Settings" component={SettingsScreen} />
     </Tab.Navigator>

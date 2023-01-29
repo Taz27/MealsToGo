a963b8b6958cd493b8d6ad58ebe1a5835b495581
Taran Mand <tsmand@yahoo.com>
2023-01-29 12:08:05 +1100

    update map screen

--- src/features/map/screens/map.screen.tsx
+++ src/features/map/screens/map.screen.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import MapView from 'react-native-maps';
+import styled from 'styled-components/native';
+import { Search } from '../components/search.component';
+
+const Map = styled(MapView)`
+  height: 100%;
+  width: 100%;
+`;
 
 export const MapScreen: React.FC = () => {
-    return (
-        <MapView style={{ height: '100%', width: '100%' }} />
-    )
-}
+  return (
+    <>
+      <Search />
+      <Map />
+    </>
+  );
+};

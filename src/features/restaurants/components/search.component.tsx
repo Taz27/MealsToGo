41fb514c0a0948e700836a94cf10e2943557c5c2
Taran Mand <tsmand@yahoo.com>
2023-01-29 16:21:47 +1100

    add icon on restaurant search bar

--- src/features/restaurants/components/search.component.tsx
+++ src/features/restaurants/components/search.component.tsx
@@ -9,11 +9,16 @@ interface ThemeProps {
   theme: Theme;
 }
 
+interface SearchProps {
+  isFavouritesToggled: boolean;
+  onFavouritesToggle: () => void;
+}
+
 const SearchContainer = styled.View`
   padding: ${(props: ThemeProps) => props.theme.space[3]};
 `;
 
-export const Search = () => {
+export const Search: React.FC<SearchProps> = ({ isFavouritesToggled, onFavouritesToggle }) => {
   const { keyword, search } = useContext(LocationContext);
   const [searchKeyword, setSearchKeyword] = useState<string>(keyword);
 
@@ -25,6 +30,8 @@ export const Search = () => {
     <SearchContainer>
       <Searchbar
         placeholder="Search for a location"
+        icon={isFavouritesToggled ? 'heart' : 'heart-outline'}
+        onIconPress={onFavouritesToggle}
         value={searchKeyword}
         onSubmitEditing={() => {
           search(searchKeyword.trim());

20ed2a494fa6e8a249d0e1e8416b169618b82eaa
Taran Mand <tsmand@yahoo.com>
2023-02-19 17:19:39 +1100

    add stack navigation type in account navigator

--- src/infra/navigation/account.navigator.tsx
+++ src/infra/navigation/account.navigator.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator, StackNavigationProp } from '@react-navigation/stack';
 
 import { AccountScreen } from '../../features/account/screens/account.screen';
 import { LoginScreen } from '../../features/account/screens/login.screen';
 import { RegisterScreen } from '../../features/account/screens/register.screen';
 
 const Stack = createStackNavigator();
+
+export type StackParamList = {
+  Main: undefined;
+  Login: undefined;
+  Register: undefined;
+};
+
+export type StackNavigation = StackNavigationProp<StackParamList>;
 
 export const AccountNavigator: React.FC = () => {
   return (

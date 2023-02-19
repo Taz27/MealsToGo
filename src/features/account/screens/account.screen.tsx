ceb054fd86d48f0ed1fd12d7f93c159012b74430
Taran Mand <tsmand@yahoo.com>
2023-02-19 17:20:39 +1100

    update account screen - add buttons

--- src/features/account/screens/account.screen.tsx
+++ src/features/account/screens/account.screen.tsx
@@ -1,11 +1,40 @@
 import React from 'react';
 
-import { AccountBackground, AccountCover } from '../components/account.styles';
+import { Spacer } from '../../../components/spacer/spacer.component';
+import { StackNavigation } from '../../../infra/navigation/account.navigator';
+import {
+  AccountBackground,
+  AccountCover,
+  AccountContainer,
+  AuthButton,
+} from '../components/account.styles';
 
-export const AccountScreen: React.FC = () => {
+interface StackNavigationProps {
+  navigation: StackNavigation;
+}
+
+export const AccountScreen: React.FC<StackNavigationProps> = ({ navigation }) => {
   return (
     <AccountBackground>
       <AccountCover />
+      <AccountContainer>
+        <AuthButton
+          icon="lock-open-outline"
+          mode="contained"
+          onPress={() => navigation.navigate('Login')}
+        >
+          Login
+        </AuthButton>
+        <Spacer size="large" position="top">
+          <AuthButton
+            icon="lock-open-outline"
+            mode="contained"
+            onPress={() => navigation.navigate('Register')}
+          >
+            Register
+          </AuthButton>
+        </Spacer>
+      </AccountContainer>
     </AccountBackground>
   );
 };
